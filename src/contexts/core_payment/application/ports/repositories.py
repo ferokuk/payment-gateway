6434ad9ecb00c9add033ff499841007b1587eaad
Yaroslave <gneushevyar@mail.ru>
2026-05-25 17:24:37 +0300

    feature(repository): исправил отступы в pyproject.toml, добавил get_by_id в абстрактный и sqlalchemy репозитории

--- src/contexts/core_payment/application/ports/repositories.py
+++ src/contexts/core_payment/application/ports/repositories.py
@@ -1,8 +1,12 @@
 from typing import Protocol
+from uuid import UUID
 
 from src.contexts.core_payment.domain.payment import Payment
 
 
 class PaymentRepository(Protocol):
     async def add(self, payment: Payment) -> None:
+        pass
+
+    async def get_by_id(self, payment_id: UUID) -> Payment | None:
         pass
--- src/contexts/core_payment/infrastructure/database/repositories.py
+++ src/contexts/core_payment/infrastructure/database/repositories.py
@@ -1,3 +1,5 @@
+from uuid import UUID
+
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.contexts.core_payment.domain.payment import Payment
@@ -12,6 +14,10 @@ class SQLAlchemyPaymentRepository:
         model = self._to_model(payment)
         self._session.add(model)
 
+    async def get_by_id(self, payment_id: UUID) -> Payment | None:
+        model = await self._session.get(PaymentModel, payment_id)
+        return self._to_domain(model) if model else None
+
     @staticmethod
     def _to_model(payment: Payment) -> PaymentModel:
         return PaymentModel(
@@ -20,6 +26,18 @@ class SQLAlchemyPaymentRepository:
             amount=payment.amount,
             currency=payment.currency,
             meta=payment.metadata,
+            status=payment.status,
+            created_at=payment.created_at,
+        )
+
+    @staticmethod
+    def _to_domain(payment: PaymentModel) -> Payment:
+        return Payment(
+            id=payment.id,
+            provider_id=payment.provider_id,
+            amount=payment.amount,
+            currency=payment.currency,
+            metadata=payment.metadata,
             status=payment.status,
             created_at=payment.created_at,
         )
